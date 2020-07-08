d54ed587721d2d1c000addad7783fd324a7d35d9
Tien Nguyen <tien.ng1020@gmail.com>
2020-07-07 23:02:44 -0500

    adding files

---
+++ chatbot.py
@@ -0,0 +1,11 @@
+you = "Hello"
+if you == "":
+    robot_brain: "I can't here you, try again"
+elif you == "Hello":
+    robot_brain = "Hello Tien"
+elif you == "today":
+    robot_brain = "Monday"
+else:
+    robot_brain = "I'm fine thank you and you"
+
+print(robot_brain)
---
+++ speak.py
@@ -0,0 +1,5 @@
+import pyttsx3
+robot_brain = "I can't hear you, try again"
+robot_mouth = pyttsx3.init()
+robot_mouth.say(robot_brain)
+robot_mouth.runAndWait()
